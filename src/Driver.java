73d28acf4a0e6edd45f7c577e84e9b8c5bb01a38
heinzja <heinzja@msoe.edu>
2017-10-08 21:17:37 -0500

    1. Added getFile, parseFile, and addFileType methods to FileManager. 2. Cleaned up imports for FileManager and Driver Classes.

--- src/Driver.java
+++ src/Driver.java
@@ -2,7 +2,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
-import java.io.File;
 
 
 /**
--- src/FileManager.java
+++ src/FileManager.java
@@ -1,5 +1,8 @@
 import java.io.*;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -11,7 +14,7 @@ public class FileManager {
 
 	private Collection<File> validFileList;
 	private static Collection<Observer> observers;
-
+	private ArrayList<String> validFileTypes;
 
 	public void FileManager(){
 
@@ -27,7 +30,7 @@ public class FileManager {
 
 	/**
 	 * Author: joseph heinz heinzja@msoe.edu
-	 * Description:
+	 * Description: current implementation only checks if first line follows format
 	 * @param file - file which is to be checked for validity
 	 */
 	private boolean isValid(File file){
@@ -36,15 +39,17 @@ public class FileManager {
 		try (InputStream in = Files.newInputStream(filePath);
 			 BufferedReader reader = new BufferedReader(new InputStreamReader(in)))
 		{
-			String line;
-			while ((line = reader.readLine()) != null) {
-					//check first line, make sure all following lines follow that format.
-				for(int i = 0; i < line.length();i++){
+			String firstLine = reader.readLine();
+			//TODO: if necessary, iterate through entire file to check for validity
 
+			for(int i=0;i<validFileTypes.size();i++){ //checks if valid file type
+				if(firstLine.equals(validFileTypes.get(i))){
+					result = true;
 				}
 			}
+
 		} catch (IOException io) {
-			System.err.println(io);
+			System.err.println("Test: File sent to isValid, not found.");
 		}
 
 		return result;
@@ -56,7 +61,7 @@ public class FileManager {
      *              If the directory 'validfiles' does not exist, its creates the directory and add the file to it.
 	 * @param file File which is to be added to validFileList
 	 */
-	public boolean addFile(File file){
+	private boolean addFile(File file){
         boolean result = false;                                                                            //initializes result, false by default
 		if(isValid(file)) {                                                                                     //checks if file is valid
             File newDir = new File("validfiles");                                                        //makes temp file with validfiles folder dir
@@ -69,11 +74,11 @@ public class FileManager {
                 }
 
                 if (!newFile.exists()) {                                                                        //checks if passed file already exists in dir
-                    Path dir = Paths.get(System.getProperty("user.dir").toString() + "/validfiles");        //if not, gets users directory to application
+                    Path dir = Paths.get(System.getProperty("user.dir") + "/validfiles");        //if not, gets users directory to application
                     Files.copy(file.toPath(), dir.resolve(file.getName()));                                      //copies file data from passed file to new directory
                     result = true;
                 } else {
-                    System.out.println("TEST: file with that name already exists");
+                    System.out.println("TEST: " + newFile.getName() + " already exists");
                 }
 
             } catch (IOException io) {
@@ -102,11 +107,25 @@ public class FileManager {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param filename
 	 */
 	public boolean rmFile(String filename){
-		return false;
+		boolean result = false;
+		//TODO: Complete rmFile
+
+		return result;
+	}
+
+	/**
+	 *
+	 * @param filename - name of the file to get from Collection validFileList
+	 * @return File - file found in list, returns NULL if file does not exist
+	 */
+	private File getFile(String filename){
+		File result = null;
+		//TODO: Complete getFile
+		return result;
 	}
 
 	/**
@@ -131,6 +150,37 @@ public class FileManager {
 	 */
 	public Trip parseTripFile(File file){
 		return null;
+	}
+
+	private boolean parseFile(File file){ //TODO: Set specific action for each file type.
+		boolean result = false;
+
+
+
+		return result; 									//returns result of if the file was parsed correctly.
+	}
+
+	/**
+	 *
+	 * @param fileTypes - file which stores a list of other file formats
+	 * @return result - true if able to add file, false if file already exists.
+	 */
+	private boolean addFileType(File fileTypes){
+
+		boolean result = false;
+		try {
+
+			if (!fileTypes.exists()) {
+				fileTypes.createNewFile();
+				result = true;
+			}else {
+				System.out.println("TEST: " + fileTypes.getName() + "Already Exists");
+			}
+
+		}catch (IOException io){
+			System.out.println("TEST: IOException -> addFileTypes");
+		}
+		return result;
 	}
 
 }
