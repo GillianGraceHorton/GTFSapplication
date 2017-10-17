e98597a55a877ac148b64ac701920adfb57c6996
hoffmanjc <hoffmanjc@msoe.edu>
2017-10-16 20:39:44 -0500

    Added tests for the RouteTest

--- Testing/RouteTest.java
+++ Testing/RouteTest.java
@@ -1,14 +1,49 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RouteTest {
-    @Test
-    void addStop() {
+    Route route;
+
+    @BeforeEach
+    void startUp() {
+        route = new Route("route_1","null","null","null",
+                "null","null","null","null","null");
     }
 
+    /**
+     * Checks if a stop can be ucessfully added
+     *
+     * :author: hoffmanjc
+     */
+    @Test
+    void addStop() {
+        //Test if all stops can be found
+        for(int i = 1; i <= 10; i++) {
+            Stop stop = new Stop(12, 11, "Stop", "stop_" + i, "Stop");
+            assertTrue(route.addStop(stop, i));
+        }
+    }
+
+    /**
+     * Checks if a stop can be successfully searched in a route
+     *
+     * :author: hoffmanjc
+     */
     @Test
     void searchRoute() {
+        //Add stops
+        for(int i = 1; i <= 10; i++) {
+            Stop stop = new Stop(12, 11, "Stop", "stop_" + i, "Stop");
+            route.addStop(stop, i);
+        }
+
+
+        //Test if all stops can be found
+        for(int i = 10; i >= 1; i--) {
+            assertEquals(route.searchRoute("stop_" + i).getStopID(), ("stop_" + i));
+        }
     }
 
 }
