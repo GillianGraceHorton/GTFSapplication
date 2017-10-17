c2cbe15132e50f1c5cf0bd52b049738ff85adaef
hortong <hortong@msoe.edu>
2017-10-16 22:40:54 -0500

    created test for ListViewTest, regenerated the controllerTest class

--- Testing/ListViewTest.java
+++ Testing/ListViewTest.java
@@ -1,13 +1,34 @@
+import javafx.stage.Stage;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ListViewTest {
+    ListView listView;
+
+    @BeforeEach
+    public void setUp(){
+        listView = new ListView();
+    }
+
     /**
      * @author: hortong
      */
     @Test
     void update() {
+        ArrayList<Object> updates = new ArrayList<>();
+        Stop stop = new Stop(0, 0, "test",  "1A", "");
+        DataStorage dataStorage = new DataStorage();
+        dataStorage.attach(listView);
+
+        dataStorage.notifyObservers(updates);
+        assertEquals(listView.getTabs().get(0).getContent(), "");
+
+        updates.add(stop);
+        assertEquals(listView.getTabs().get(0).getContent(), stop.toString());
     }
 
 }
