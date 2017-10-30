f85d1ae0b12b9cec58217bac1392484117c5a826
heinzja <heinzja@msoe.edu>
2017-10-30 17:20:18 -0500

    added SimpleTimer class for future time testing.

---
+++ src/SimpleTimer.java
@@ -0,0 +1,27 @@
+public class SimpleTimer {
+    private double startTime;
+    private double endTime;
+
+    public SimpleTimer(){
+        this.startTime = 0;
+        this.endTime = 0;
+    }
+
+    public void start(){
+        this.startTime = System.nanoTime();
+    }
+    public void end(){
+        this.endTime = System.nanoTime();
+    }
+    private double getEndTime(){
+        return this.endTime;
+    }
+
+    private double getStartTime(){
+        return this.startTime;
+    }
+
+    public void result(){
+        System.out.printf("%.6f seconds\n",(this.endTime - this.startTime)/1000000000);
+    }
+}
